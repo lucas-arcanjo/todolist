e6dcf440222074e287c20d7463a0647334809193
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2026-01-22 20:37:57 -0300

    feat: adding new project

---
+++ src/main/java/br/com/lucasarcanjo/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.lucasarcanjo.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String name;
+    private String username;
+    private String password;
+
+}
---
+++ src/test/java/br/com/lucasarcanjo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasarcanjo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
